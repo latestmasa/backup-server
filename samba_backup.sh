1b1ade1b9894326f51fb9495c1f93f509d08db20
masa <masasam@users.noreply.github.com>
2017-03-03 15:06:18 +0900

    In English

--- samba_backup.sh
+++ samba_backup.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 # samba_backup.sh
-# sambaサーバ用backupスクリプト
 
 MAILTO=yourmailaddress@example.com
 
-##################################### 使い方 #######################################################
-# $1 sambaサーバのドメインを指定する
-# $2 sambaサーバのログインuser名を指定する
-# $3 backupを保存する世代を指定
-#
-# -r [rsyncの転送レート(単位はKB/秒)]
-#    例: -r 200 (これで200K/s)
-#    -rがない場合は100KB/秒(これで帯域1Mくらいに制限できる)
-# -c rsyncで本番サイトのCPU負荷とIO負荷を下げたいときにcを指定する
-#    何もない場合はベストエフォートでrsyncが動く
-####################################################################################################
-# バックアップ方法を指定する(圧縮率はデータによる)
-# afz     安全性重視 backupデータが損傷を受けても損傷を受けた場所以外は修復できる 圧縮率85% CPU負荷10% 圧縮時間2倍
-#       解凍方法 cd backup/example.com;
-#          afio -ivZ /backup/example.com/ctdev.new.afz
-#       必要条件:yum install afio
-# tar.gz  unix標準 backupデータが損傷を受けるとそれ以降のデータは保証されない 以下tarを使うのは同じ問題をもつ
-# tar.7z  圧縮率重視 65%程度 圧縮時間6.5倍 CPU負荷大 メモリー消費74倍(tar.gz比)
-#       解凍方法 cd backup/example.com;
-#          7za x -so ctdev.new.tar.7z | tar xf -
-#       必要条件:yum install p7zip
-# tar.bz2 圧縮率は2番目に良い 圧縮時間2.5倍 メモリ消費8倍(tar.gz比)
+# $1 samba domain
+# $2 samba server's ssh login user
+# $3 Specify generation to save backup
 ext=tar.gz
 
 BACKUP_DIR=${PWD}/backup
@@ -66,7 +46,7 @@ if [ ! -d $RSYNC_DIR/$1 ]; then
 fi
 
 
-# バックアップ時間を保存
+# Save backup time
 find /tmp/ -daystart -mtime -1 -type f -name backuptime | grep backuptime > /dev/null
 if [ $? = 0 ]; then
   echo "$1 samba transfer start "`date +%Y"-"%m"-"%d" "%H':'%M':'%S` >> /tmp/backuptime
@@ -110,14 +90,14 @@ case $ext in
 
 esac
 
-# backupに失敗したら管理者にメールして該当データのローテート処理をとばす(データ保護のため)
+# If backup fails, e-mail the administrator and skip the rotation process of the corresponding data
 if [ $? != 0 ]; then
   echo "$1 samba rsync backup samba.new.$ext failure!" | mail -s $0 $MAILTO
   exit 1
 fi
 
 
-# 正常に終了したらローテートする
+# Rotate after completing normally
 gens=`expr $3 - 1`
 
 while [ $gens -gt 1 ]; do
@@ -135,7 +115,7 @@ if [ -f $archive ]; then
   mv -f $archive $archive2
 fi
 
-# ローテート終了後最新版newを正式名称に変更する
+
 mv -f $BACKUP_DIR/$1/samba.new.$ext $BACKUP_DIR/$1/samba.$ext
 
 echo "$1 samba transfer end "`date +%Y"-"%m"-"%d" "%H':'%M':'%S` >> /tmp/backuptime
